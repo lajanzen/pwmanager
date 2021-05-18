f4c117ce507c99abc0bc9d0fe6010ec203906247
lajanzen <lara-janzen@hotmail.com>
2021-05-18 14:32:00 +0200

    Add save credential in MongoDB Feature

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,15 +1,10 @@
 import fs from "fs/promises";
+import { Cursor } from "mongodb";
 import { Credential } from "../types";
 import { getCollection } from "./database";
 
-type DB = {
-  credentials: Credential[];
-};
-
-export const readCredentials = async (): Promise<Credential[]> => {
-  const result = await fs.readFile("./db.json", "utf-8");
-  const data: DB = JSON.parse(result);
-  return data.credentials;
+export const readCredentials = async (): Promise<Map> => {
+  await getCollection("credentials").find();
 };
 
 export const saveCredentials = async (
