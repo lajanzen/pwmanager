a1e4f05a652acca1aeb80754685d2a2b6dd75d25
lajanzen <lara-janzen@hotmail.com>
2021-05-18 14:57:57 +0200

    Add read credentials from MongoDB feature

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,14 +1,20 @@
-import fs from "fs/promises";
-import { Cursor } from "mongodb";
+// import fs from "fs/promises";
 import { Credential } from "../types";
 import { getCollection } from "./database";
+import CryptoJS from "crypto-js";
 
-export const readCredentials = async (): Promise<Map> => {
-  await getCollection("credentials").find();
+export const readCredentials = async (): Promise<Credential[]> => {
+  const cursor = getCollection("credentials").find();
+  return await cursor.toArray();
 };
 
 export const saveCredentials = async (
   newCredential: Credential
 ): Promise<void> => {
+  const encryptedPassword = CryptoJS.AES.encrypt(
+    newCredential.password,
+    "bla"
+  ).toString();
+  newCredential.password = encryptedPassword;
   await getCollection("credentials").insertOne(newCredential);
 };
