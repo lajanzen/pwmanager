8662677f72a651243c5da108c6d52a03321e4c55
lajanzen <lara-janzen@hotmail.com>
2021-05-19 13:41:52 +0200

    Connect to database and return all credentials

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import { readCredentials } from "./utils/credentials";
 import { connectDatabase } from "./utils/database";
 
 if (process.env.MONGO_URL === undefined) {
@@ -11,8 +12,9 @@ if (process.env.MONGO_URL === undefined) {
 const app = express();
 const port = 5000;
 
-app.get("/api/credentials", (_request, response) => {
-  response.send("All credentials requested");
+app.get("/api/credentials", async (_request, response) => {
+  const credentials = await readCredentials();
+  response.json(credentials);
 });
 
 app.post("/api/credentials", (_request, response) => {
