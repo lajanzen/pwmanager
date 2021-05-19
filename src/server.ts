8cddee031f3369bc3b5aef0b7b9077822205aae0
lajanzen <lara-janzen@hotmail.com>
2021-05-19 15:39:42 +0200

    Install tsnode and add save credential feature

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import { readCredentials } from "./utils/credentials";
+import { readCredentials, saveCredentials } from "./utils/credentials";
 import { connectDatabase } from "./utils/database";
 
 if (process.env.MONGO_URL === undefined) {
@@ -12,13 +12,17 @@ if (process.env.MONGO_URL === undefined) {
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 app.get("/api/credentials", async (_request, response) => {
   const credentials = await readCredentials();
   response.json(credentials);
 });
 
-app.post("/api/credentials", (_request, response) => {
-  response.send("Add new credential");
+app.post("/api/credentials", async (request, response) => {
+  const credentials = await request.body;
+  saveCredentials(credentials);
+  response.json(request.body);
 });
 
 connectDatabase(process.env.MONGO_URL).then(() => {
